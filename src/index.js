5861f6a1d1e5c79e7e3df0091475f87161471854
Bill Nguyen <billfl93@gmail.com>
2022-08-19 00:18:43 -0400

    commit tree 71036edf9b41149c7c6d7536b73914a27a596e2f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let breeds = []
+const ul = document.querySelector("#dog-breeds")
+
+fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(data => data.message.forEach(dogImage => renderImage(dogImage)))
+
+function renderImage(dogImage) {
+  const container = document.querySelector("#dog-image-container")
+  const image = document.createElement('img')
+  image.src = dogImage
+  container.append(image)
+  
+}   
+
+fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(data => {
+      breeds = Object.keys(data.message)
+      renderBreeds(breeds)
+  })
+
+function renderBreeds(breeds) {
+  
+  breeds.forEach(breed => {
+    const li = document.createElement('li')
+    li.innerText = breed
+    ul.append(li)
+    li.addEventListener('click', changeColor)
+  })
+}
+
+function changeColor(e) {
+  e.target.style.color = "red"
+}
+
+const dropdown = document.getElementById('breed-dropdown')
+dropdown.addEventListener('change', handleChange)
+
+function handleChange(e) {
+  let letter = e.target.value
+  let filteredBreeds = breeds.filter(breeds => breeds.startsWith(letter))
+  ul.textContent = ''
+  renderBreeds(filteredBreeds)
+}
+
+})
